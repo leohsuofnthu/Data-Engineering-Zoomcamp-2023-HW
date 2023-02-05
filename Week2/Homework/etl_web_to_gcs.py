b98b6a2a8382d591034dbab26aaa87164315ddf9
leohsuofnthu <leohsuofnthu@gmail.com>
2023-02-04 22:45:04 -0500

    create dir if not exist

--- Week2/Homework/etl_web_to_gcs.py
+++ Week2/Homework/etl_web_to_gcs.py
@@ -34,6 +34,10 @@ def clean(df: pd.DataFrame, color: str) -> pd.DataFrame:
 @task()
 def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     """Write DataFrame out locally as parquet file"""
+    dir_path = f"data/{color}/"
+    if not os.path.exists(dir_path):
+        os.makedirs(dirpath)
+       
     path = Path(f"data/{color}/{dataset_file}.parquet")
     df.to_parquet(path, compression="gzip")
     return path
